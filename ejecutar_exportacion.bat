d9280b02516a3690b07ca57efe45be1bc83a19b5
Diego Hermilo Guillen Garcia <diego@kali-Linux.Churr00.com>
2025-05-08 09:30:32 -0600

    Agregar scripts de automatización para Windows y Linux

---
+++ ejecutar_exportacion.bat
@@ -0,0 +1,4 @@
+@echo off
+echo Ejecutando exportación desde MongoDB a CSV...
+python "C:\Users\diego\Documentos\Tecmilenio\2doSemestre\DataScience\Proyecto_Final\monitoreo_tweets\mongo_to_csv.py"
+pause
---
+++ ejecutar_exportacion.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "Ejecutando exportación desde MongoDB a CSV..."
+source venv/bin/activate
+python mongo_to_csv.py
